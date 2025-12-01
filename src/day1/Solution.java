0d8ad213cf7e572593eab77cef99ba2d105ee7d3
BrianNixDesktop <briannormant@gmail.com>
2025-12-01 11:44:14 -0500

    day1: Part 1 java

--- src/day1/Solution.java
+++ src/day1/Solution.java
@@ -4,26 +4,31 @@ public class Solution {
 	public String solve(String input) {
 		var list = new LinkedList<Integer>();
 		list.add(50);
-		var r = input.lines()
-			.map(l -> {
-				int sign = (l.charAt(0) == 'R')?1:-1;
-				int num = Integer.parseInt(
-					l.substring(1)
-				);
-				return num * sign;
-			})
-			.reduce(list,
-				(dials, d) -> {
-					var l = new LinkedList<Integer>();
-					l.add(dials.getLast() + d);
-					return l;
-				},
-				(l1, l2) -> {
-					l1.addAll(l2);
-					return l1;
-				}
+		var result = input.lines()
+		.map(l -> {
+			int sign = (l.charAt(0) == 'R')?1:-1;
+			int num = Integer.parseInt(
+				l.substring(1)
 			);
+			return num * sign;
+		})
+		.reduce(list,
+		(dials, d) -> {
+				var l = new LinkedList<Integer>(dials);
+				var n = (dials.getLast() + d) % 100;
+				if (n < 0) {n += 100;}
+				l.add(n);
+				return l;
+			},
+		(l1, l2) -> {
+				var r = new LinkedList<>(l1);
+				r.addAll(l2);
+				return r;
+			}
+		).stream()
+		.filter(n -> n == 0)
+		.count();
 
-		return r.toString();
+		return "" + result;
 	}
 }
