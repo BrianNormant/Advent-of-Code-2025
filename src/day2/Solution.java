9112ebc3cf33c2dfd258a3d1bad2ce3781568360
BrianNixDesktop <briannormant@gmail.com>
2025-12-02 17:50:33 -0500

    Day2: Java part 2

--- src/day2/Solution.java
+++ src/day2/Solution.java
@@ -37,6 +37,37 @@ public class Solution {
 		return result + "";
 	}
 	public String solve2(String input) {
-		return input;
+		final String r1 = "^(\\d+)\\1+$";
+		final String r2 = "(\\d+)-(\\d+)";
+		var p1 = Pattern.compile( r1 );
+		var p2 = Pattern.compile(r2);
+
+		long result = 0;
+
+		while (!input.equals("")) {
+			var idx = input.indexOf(",");
+			String sub;
+			if (idx != -1) {
+				sub = input.substring(0, idx);
+				input = input.substring(idx+1);
+			} else {
+				sub = input;
+				input = "";
+			}
+
+			var m = p2.matcher(sub);
+			m.find();
+			long n1 = Long.parseLong(m.group(1)),
+				 n2 = Long.parseLong(m.group(2));
+
+			for (long i = n1; i <= n2; i++) {
+				var m1 = p1.matcher("" + i);
+				m1.find();
+				if (m1.hasMatch()) {
+					result += i;
+				}
+			}
+		}
+		return result + "";
 	}
 }
